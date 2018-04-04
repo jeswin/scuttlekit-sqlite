d9c91b6235d41583d707913e1f8b1db70b73ac29
Jeswin Kumar <jeswinpk@agilehead.com>
2018-04-05 00:17:34 +0530

    WIP Host Events

--- src/ssb-log.ts
+++ src/ssb-log.ts
@@ -12,9 +12,10 @@ import {
   ITableSchema,
   Operation
 } from "./types";
+import { NonResult } from "./host-events";
 
 export async function mergeMessagesIntoRow(
-  msg: Msg<ILogEntry<IRowMeta>>,
+  message: Msg<ILogEntry<IRowMeta>>,
   table: string,
   primaryKey: string,
   db: SqliteDb,
@@ -27,66 +28,87 @@ export async function mergeMessagesIntoRow(
   const messages = sortMessages(unorderedMessages);
 
   const rowsForKey = await sql.getByPrimaryKey(table, primaryKey, db);
-
   const existingRow = rowsForKey.length > 0 ? rowsForKey.rows[0] : undefined;
-  const existsInDb = typeof existingRow === "undefined";
 
-  const logEntry = msg.value.content;
+  const logEntry = message.value.content;
 
   function loop(
-    row: IDbRow | undefined,
+    row: IDbRow | MergeToDelete | undefined,
     msgs: Msg<ILogEntry<IRowMeta>>[]
   ): MergeResult | undefined {
-    const [current, ...rest] = msgs;
-    return rest.length
-      ? logEntry.__meta.operation === Operation.Insert
-        ? // If row exists or has an invalid key, skip.
-          !row && isValidPrimaryKey()
-          ? loop(insert(current, existsInDb), rest)
-          : loop(row, rest)
-        : logEntry.__meta.operation === Operation.Update
-          ? row && hasUpdatePermissions()
-            ? loop(update(current, existsInDb), rest)
-            : // If row does not exist or no permissions, skip.
-              loop(row, rest)
-          : logEntry.__meta.operation === Operation.Del
-            ? // Delete if row exists in db and you have delete permissions
-              existsInDb && hasDeletePermissions()
-              ? new MergeToDelete(primaryKey)
-              : // if row not in db and was just created, return empty.
-                row
-                ? undefined
-                : // If row doesn't exist, skip.
-                  loop(row, rest)
-            : loop(row, rest)
-      : row
-        ? existsInDb ? new MergeToUpdate(row) : new MergeToInsert(row)
-        : undefined;
+    if (row instanceof MergeToDelete) {
+      return row;
+    } else {
+      const [current, ...rest] = msgs;
+      return rest.length
+        ? logEntry.__meta.operation === Operation.Insert
+          ? // If row exists or has an invalid key, skip.
+            !row ? loop(insert(current, row) || row, rest) : loop(row, rest)
+          : logEntry.__meta.operation === Operation.Update
+            ? row
+              ? loop(update(current, row) || row, rest)
+              : // If row does not exist, skip.
+                loop(row, rest)
+            : logEntry.__meta.operation === Operation.Del
+              ? row ? del(current, row) || loop(row, rest) : loop(row, rest)
+              : loop(row, rest)
+        : row
+          ? existingRow ? new MergeToUpdate(row) : new MergeToInsert(row)
+          : undefined;
+    }
   }
 
   return loop(existingRow, messages);
 }
 
+/*
+  Returns undefined if the row already exists.
+*/
 function insert(
   msg: Msg<ILogEntry<IRowMeta>>,
-  rowExists: boolean
+  row?: IDbRow
 ): IDbRow | undefined {
-  const logEntry = msg.value.content;
-  const [rowId, feedId] = logEntry.__meta.primaryKey.split("_");
-  return !rowExists && feedId === msg.value.author
-    ? constructRowFromMessage(
-        logEntry as ILogEntry<IEditMeta>,
-        msg.value.timestamp
-      )
-    : undefined;
+  if (!row) {
+    const logEntry = msg.value.content;
+    const [rowId, feedId] = logEntry.__meta.primaryKey.split("_");
+    return feedId === msg.value.author
+      ? constructRowFromMessage(
+          logEntry as ILogEntry<IEditMeta>,
+          msg.value.timestamp
+        )
+      : undefined;
+  } else {
+    return undefined;
+  }
 }
 
-function update(msg: Msg<ILogEntry<IRowMeta>>, row: IDbRow): void {
-  const logEntry = msg.value.content;
+/*
+  Returns undefined if row doesn't exist.
+*/
+function update(
+  msg: Msg<ILogEntry<IRowMeta>>,
+  row: IDbRow
+): IDbRow | undefined {
+  const permissions = getPermissionsFromString(row.__permissions);
+  if (row) {
+    return row;
+  } else {
+    return undefined;
+  }
 }
 
-function del(msg: Msg<ILogEntry<IRowMeta>>, row: IDbRow): void {
-  const logEntry = msg.value.content;
+/*
+  Returns undefined if row doesn't exist.
+*/
+function del(
+  msg: Msg<ILogEntry<IRowMeta>>,
+  row: IDbRow
+): MergeToDelete | undefined {
+  if (row) {
+    return new MergeToDelete(primaryKey);
+  } else {
+    return undefined;
+  }
 }
 
 export class MergeToDelete {
@@ -121,6 +143,18 @@ function sortMessages(entries: Msg<ILogEntry<IRowMeta>>[]) {
 function parsePrimaryKey<T>(msg: Msg<ILogEntry<IEditMeta>>): [string, string] {
   const [rowId, feedId] = msg.value.content.primaryKey.split("_");
   return [rowId, feedId];
+}
+
+export function getPermissionsFromString(strPerms?: string) {
+  return strPerms
+    ? strPerms.split(";").map(f => {
+        const [feedId, strFields] = f.split(":");
+        return {
+          feedId,
+          fields: strFields.split(",")
+        };
+      })
+    : [];
 }
 
 export function getPermissionsField(logEntry: ILogEntry<IEditMeta>) {
